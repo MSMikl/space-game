7e96e5ad84f179e0ea433801f89df73ea784c366
MSMikl <michalbl4@gmail.com>
2022-11-02 15:43:36 +0600

    Coroutines collected in one list

--- main.py
+++ main.py
@@ -65,6 +65,8 @@ async def render_spaceship(canvas, column, row, frames):
     # Максимальные координаты окна на единицу меньше размера, поскольку нумерация начинается с 0
     max_y = size[0] - 1
     max_x = size[1] - 1
+    ship_width = 6
+    ship_length = 8
     for frame in cycle(frames):
         if space:
             break
@@ -76,8 +78,8 @@ async def render_spaceship(canvas, column, row, frames):
         step_y += y
         await asyncio.sleep(0)
         draw_frame(canvas, row, column, frame, negative=True)
-        column = min(column + step_x, max_x - 6) if step_x >= 0 else max(column + step_x, 1)
-        row = min(row + step_y, max_y - 10) if step_y >= 0 else max(row + step_y, 1)
+        column = min(column + step_x, max_x - ship_width) if step_x >= 0 else max(column + step_x, 1)
+        row = min(row + step_y, max_y - ship_length) if step_y >= 0 else max(row + step_y, 1)
 
 
 def draw(canvas):
@@ -89,12 +91,7 @@ def draw(canvas):
         if isfile(full_path):
             with open(full_path) as file:
                 frames.append(file.read())
-    multipled_frames = [frame for frame in frames for _ in range(2)]
-    print(multipled_frames)
-    curses.curs_set(False)
     y, x = canvas.getmaxyx()
-    shot = fire(canvas, y//2, x//2)
-    animation = render_spaceship(canvas, 5, 5, multipled_frames)
     coroutines = [blink(
         canvas,
         row=random.randint(1, y-1),
@@ -102,16 +99,19 @@ def draw(canvas):
         symbol=random.choice('+*.:'),
         offset_tics=random.randint(1, 3),
         ) for _ in range (random.randint(50, 100))]
-    is_shot = True
+    
+    multipled_frames = [frame for frame in frames for _ in range(2)]
+    print(multipled_frames)
+    curses.curs_set(False)
+    shot = fire(canvas, y//2, x//2)
+    coroutines.append(shot)
+    coroutines.append(render_spaceship(canvas, 5, 5, multipled_frames))
     while True:
-        for coroutine in coroutines:
-            coroutine.send(None)
-        if is_shot:
+        for coroutine in coroutines.copy():
             try:
-                shot.send(None)
+                coroutine.send(None)
             except StopIteration:
-                is_shot = False
-        animation.send(None)
+                coroutines.remove(shot)
 
         canvas.refresh()
         time.sleep(0.1)
